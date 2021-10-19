f4d7155c3f47243a89ce0918493869e781cc3298
ZumTheZazaKing <zahidi85543@gmail.com>
2021-10-19 23:04:32 +0800

    Functions all added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { lazy, Suspense, useReducer, } from 'react';
 import { Context } from '../data/context';
+import { winningArrays } from '../data/winningArrays';
 
 const Main = lazy(() => import('./Main').then(module => ({default:module.Main})));
 
@@ -14,6 +15,18 @@ function App() {
           ...state,
           board:action.board,
           currentPlayer:action.nextPlayer
+        }
+
+      case "endGame":
+        return {
+          ...state,
+          gameOver:true,
+          message:action.message
+        }
+
+      case "newGame":
+        return{
+          ...initialGameState
         }
 
         default:
@@ -39,24 +52,68 @@ function App() {
     message:''
   }
 
-  const [gameState, dispatchGameState] = useReducer(gameReducer, initialGameState)
+  const [gameState, dispatchGameState] = useReducer(gameReducer, initialGameState);
+
+  const newGame = () => {
+    dispatchGameState({type:"newGame"})
+  }
+
+  const checkWin = (board) => {
+
+    for (var i = 0; i < winningArrays.length; i++){
+      const square1 = board[winningArrays[i][0]];
+      const square2 = board[winningArrays[i][1]];
+      const square3 = board[winningArrays[i][2]];
+      const square4 = board[winningArrays[i][3]];
+
+      if(
+        square1 === "player-one" &&
+        square2 === "player-one" &&
+        square3 === "player-one" &&
+        square4 === "player-one"
+      ){
+        dispatchGameState({type:"endGame", message:"Player One Wins"});
+      }
+      if(
+        square1 === "player-two" &&
+        square2 === "player-two" &&
+        square3 === "player-two" &&
+        square4 === "player-two"
+      ){
+        dispatchGameState({type:"endGame", message:"Player Two Wins"});
+      }
+
+    }
+
+  }
 
   const play = e => {
+
+    if(gameState.gameOver)return;
 
     let board = [...gameState.board];
     let nextPlayer = gameState.currentPlayer === gameState.player1
       ? gameState.player2
       : gameState.player1
 
+    let index = parseInt(e.target.id);
+    let validAddition = 7;
+
+    while(board[index + validAddition] === ""){
+      index += validAddition;
+    }
+
     if(e.target.className === "token "){
+
       if(gameState.currentPlayer === 1){
-        board[e.target.id] = "player-one";
+        board[index] = "player-one";
 
       } else {
-        board[e.target.id] = "player-two";
+        board[index] = "player-two";
       }
       dispatchGameState({type:"togglePlayer", board, nextPlayer});
     }
+    checkWin(board);
   }
 
   
@@ -70,7 +127,7 @@ function App() {
       <Suspense fallback={<h1>Loading...</h1>}>
         <Context.Provider value={{
           gameState, dispatchGameState,
-          play
+          play, newGame
         }}>
 
         <Main/>
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,13 +5,17 @@ import { Cell } from './Cell';
 
 export function Main(){
 
-    let { gameState, play } = useContext(Context);
+    let { gameState, play, newGame } = useContext(Context);
 
     return (<div id="Main">
+
+        <button onClick={() => newGame()} className={gameState.gameOver ? "" : "hidden"}>New Game</button>
 
         <div id="board">
             {gameState.board && gameState.board.map((row, i) => <Cell play={play} cellNumber={i} value={row} key={i}/>)}
         </div>
+
+        <h2>{gameState.message}</h2>
 
     </div>)
 }
